fe94a99efce9016bda5256ee2c4e42987b524334
Shaun Kiszonas <smkiszonas@gmail.com>
2022-03-09 16:31:28 -0800

    Refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,29 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-function createTaskHander(event) {
+function taskFormHander(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("Input[name='task-name']").value;
     var taskTypeInput = document.querySelector("Select[name='task-type']").value;
 
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    createTaskEl(taskDataObj);
+}
+
+function createTaskEl(taskDataObj) {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
 
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
 
     listItemEl.appendChild(taskInfoEl);
     tasksToDoEl.appendChild(listItemEl);
 }
 
-formEl.addEventListener("submit", createTaskHander);
+formEl.addEventListener("submit", taskFormHander);
